38de1b958e3a85f0d2b90e72bcb80949099ad074
baojiwei <baojiwei@baojiwei.com>
2015-11-19 07:55:03 +0800

    change something

--- hello.py
+++ hello.py
@@ -1,23 +1,26 @@
-from flask import Flask
-from flask.ext.script import Manager
+from flask import Flask, render_template
+#from flask.ext.script import Manager
 from flask import make_response
 from flask import redirect
 from flask import abort
 from flask import request
+from flask.ext.bootstrap import Bootstrap
+
 
 app = Flask(__name__)
-manager = Manager(app)
+#manager = Manager(app)
 
 @app.route('/')
 def index_test():
     # to void too many arguments in view , we can use request
-    user_agent = request.headers.get('User_Agent')
-    return '<h1> Hello World! Your browser is %s</h1>' % user_agent
+    #user_agent = request.headers.get('User_Agent')
+    #return '<h1> Hello World! Your browser is %s</h1>' % user_agent
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user_test(name):
     # get string from URI
-    return '<h1>Hello, %s</h1>' % name
+    return render_template('user.html',name=name)
 
 @app.route('/null')
 def null_test():
@@ -25,9 +28,10 @@ def null_test():
 
 @app.route('/response')
 def response_test():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer','42')
-    return response
+    #response = make_response('<h1>This document carries a cookie!</h1>')
+    #response.set_cookie('answer','42')
+    #return response
+
 
 @app.route('/redirect')
 def redirect_test():
@@ -40,4 +44,5 @@ def abort_test(id):
     return '<h1>Hello,%s</h1>'%id
 
 if __name__=='__main__':
-    manager.run()
+    #manager.run()
+    app.run()
