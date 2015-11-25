7d9205b6e94c3a23e998db7b0139a51bba2cd139
baojiwei <baojiwei@baojiwei.com>
2015-11-26 00:27:09 +0800

    Add sqlite database     Understand the useage of sqlalchemy

--- hello.py
+++ hello.py
@@ -1,48 +1,74 @@
-from flask import Flask, render_template
-#from flask.ext.script import Manager
-from flask import make_response
-from flask import redirect
-from flask import abort
-from flask import request
+import os
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
 manager = Manager(app)
-
-@app.route('/')
-def index_test():
-    # to void too many arguments in view , we can use request
-    #user_agent = request.headers.get('User_Agent')
-    #return '<h1> Hello World! Your browser is %s</h1>' % user_agent
-    return render_template('index.html')
-
-@app.route('/user/<name>')
-def user_test(name):
-    # get string from URI
-    return render_template('user.html',name=name)
-
-@app.route('/null')
-def null_test():
-    return '<h1>Bad Request</h1>', 400
-
-@app.route('/response')
-def response_test():
-    #response = make_response('<h1>This document carries a cookie!</h1>')
-    #response.set_cookie('answer','42')
-    #return response
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
 
 
-@app.route('/redirect')
-def redirect_test():
-    return redirect('http://baidu.com')
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
-@app.route('/usererror/<id>')
-def abort_test(id):
-    if(id!='1'):
-        abort(404)
-    return '<h1>Hello,%s</h1>'%id
+    def __repr__(self):
+        return '<Role %r>' % self.name
 
-if __name__=='__main__':
-    #manager.run()
-    app.run()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
+
+if __name__ == '__main__':
+    db.create_all()
+    manager.run()
